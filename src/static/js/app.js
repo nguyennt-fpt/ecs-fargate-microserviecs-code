7c0a418a50724e3473ad6af65a4b1e964726ca21
NguyenNT-FPT <jamesnguyen.ed@gmail.com>
2025-11-22 18:57:18 +0700

    change app.js2

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above! new versionggg</p>
+                <p className="text-center">No items yet! Add one above! new versiosdfadsfasdfsadfsdfnggg</p>
             )}
             {items.map(item => (
                 <ItemDisplay
